688a283007cadff353b483bfdbb965c7c7907305
yeeuniii <anna862700@gmail.com>
2021-11-05 03:56:37 +0900

    level4(1)

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import turtle as t
 import random as r
 
-num=int(input("몇마리의 거북이?"))
+
 set={}
-for i in range(1,num+1):
-    set[i]=t.Turtle()
+while True:
+    name = input("거북이 이름을 입력하시오.")
+    if name == "":
+        break
+    else:
+        set[name] = t.Turtle()
+
+turtlename=list(set.keys())
 turtles=list(set.values())
+turtlenum=len(set)
 
 def go(t,y):
     t.penup()
     t.goto(-200,y)
     t.pendown()
 
-trial=int(input("시도 횟수"))
-for i in range(num):
+trial=int(input("시도 횟수를 입력하시오"))
+for i in range(turtlenum):
     turtles[i].shape("turtle")
-    go(turtles[i],100-200/(num-1)*i)
+    go(turtles[i],100-200/(turtlenum-1)*i)
+    turtles[i].write(turtlename[i])
     for j in range(1, trial + 1):
         turtles[i].forward(10)
-
-
-
-
-
 
 t.mainloop()
 
